fce3253a879ceb74994534d9c1b635b20e34f749
velirean <47877809+velirean@users.noreply.github.com>
2021-03-12 21:59:40 -0600

    Ahora registra la tarea mediante AJAX

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -8,7 +8,7 @@
 </div>
 <div class="row">
     <div class="col-12">
-        <input type="text" name="description">
+        <input type="text" name="description" id="description">
         <input type="button" onClick="createTask()" value="Crear">
     </div>
 </div>
@@ -40,7 +40,25 @@
 <script>
     function createTask()
     {
-        console.log("Crea la tarea");
+        let theDescription = $('#description').val();
+        $.ajax({
+            url: '{{ route('tasks.store') }}',
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            },
+            data: {
+                description: theDescription
+            }
+        })
+        .done(function(response) {
+            console.log('Éxitoso', response);
+        })
+        .fail(function(jqXHR, response) {
+            console.log('Fallido', response);
+        });
+
     }
 </script>
 @endpush
