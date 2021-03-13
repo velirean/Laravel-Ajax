b2b845784240994011b469389a48ad327f81d24b
velirean <47877809+velirean@users.noreply.github.com>
2021-03-12 21:03:52 -0600

    Se aplica bootstrap con botón para crear una tarea

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -1,5 +1,16 @@
 @extends('layouts.main')
 
 @section('content')
-<h1> Content</h1>
+<div class="row">
+    <div class="col-12">
+        <h1>Lista de tareas</h1>
+    </div>
+</div>
+<div class="row">
+    <div class="col-12">
+        <a class="btn btn-default" href="{{ route('tasks.create') }}">
+            Crea una tarea
+        </a>
+    </div>
+</div>
 @endsection
